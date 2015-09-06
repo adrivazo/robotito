6a777173d028c7f55923c3bf1242bf7b164b801c
Adriana <adrivazo@gmail.com>
2015-09-05 21:43:11 -0400

    we can write to slack

--- lilrobot.js
+++ lilrobot.js
@@ -19,24 +19,17 @@ app.use(function (err, req, res, next){
 
 app.post('/hello', hellobot);
 
-app.get('/sendmessage', function (){
-    var request = require('request');
-    request({
-        url:'https://hooks.slack.com/services/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',
-        method:"POST",
-        json:true,
-        headers:{
-        "content-type":"application/json"},
-        body: JSON.stringify(
-     {        payload: {
-                    text:"hello world!!"
-                }
-            })
-        ,
-        function (error, response, body){
-            console.log(response);
-        }
-    );
+var request = require('request');
+app.post('/sendmessage', function (req, res){
+    console.log("hello dudddeee");
+    request.post({
+        url: 'https://hooks.slack.com/services/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',
+        form: {payload: JSON.stringify({text: 'meow'})}
+    }, function(err,httpResponse,body) {
+        console.log("back", body);
+    });
+
+    res.status(200).send('Sent!');
 });
 
 app.listen(port, function(){
