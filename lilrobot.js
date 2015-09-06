2e23843010b5d589140c7c62a9449bad6dacc057
Adriana <adrivazo@gmail.com>
2015-09-05 21:17:39 -0400

    fixed request vn

--- lilrobot.js
+++ lilrobot.js
@@ -21,15 +21,18 @@ app.post('/hello', hellobot);
 
 app.get('/sendmessage', function (){
     var request = require('request');
-    request.post(
-        'https://hooks.slack.com/services/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',
-        {
-            form: {
-                payload: {
+    request({
+        url:'https://hooks.slack.com/services/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',
+        method:"POST",
+        json:true,
+        headers:{
+        "content-type":"application/json"},
+        body: JSON.stringify(
+     {        payload: {
                     text:"hello world!!"
                 }
-            }
-        },
+            })
+        ,
         function (error, response, body){
             console.log(response);
         }
