4b786c8cd1d59f0ccaf9a114b6a6f2270264a6ca
Adriana <adrivazo@gmail.com>
2015-09-06 01:28:21 -0400

    trying to read commands from spark core in noda app

--- hellobot.js
+++ hellobot.js
@@ -1,5 +1,8 @@
 module.exports = function (req, res, next) {
   var userName = req.body.user_name;
+  var command = req.body.data;
+    log("let's see if there was a command");
+    log(command);
   var botPayload = {
     text : 'Hi, ' + userName + '!'
   };
